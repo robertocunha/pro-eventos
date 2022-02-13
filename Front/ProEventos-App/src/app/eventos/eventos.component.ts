f82544407c9b47ce7031e751fb7fc86b8613174c
Roberto Cunha <robertocunha@gmail.com>
2022-02-13 11:08:02 -0300

    [Feat] Add images and filter

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public filteredEventos: any = [];
+
+  imageWidth: number = 150;
+  imageMargin: number = 2;
+  imageCollapsed: boolean = false;
+  private _filter: string = '';
+
+  public get filter(): string {
+    return this._filter;
+  }
+
+  public set filter(value: string) {
+    this._filter = value;
+    this.filteredEventos = this.filter ? this.filterEventos(this.filter) : this.eventos;
+  }
+
+  filterEventos(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string; }) =>
+      evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+      )
+    }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +40,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public toggleImageVisibility(): void {
+    this.imageCollapsed = !this.imageCollapsed;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.filteredEventos = this.eventos;
+      },
       error => console.log(error)
     );
   }
